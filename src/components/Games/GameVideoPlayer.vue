d7d061794abd29b1384e358149415db2eef50be6
LeonAngelit <leo63582@gmail.com>
2025-07-27 15:20:17 +0200

    commit files

---
+++ src/components/Games/GameVideoPlayer.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+defineProps({
+  video: {
+    type: String,
+    required: true,
+  },
+})
+const videoRef = ref();
+
+onMounted(() => {
+  videoRef.value.play()
+})
+</script>
+
+<template>
+  <div class="game-video-player">
+    <video controls ref="videoRef">
+      <source :src="video" type="video/mp4" />
+    </video>
+  </div>
+</template>
+
+<style scoped>
+.game-video-player {
+  max-width: 100%;
+  overflow: hidden;
+}
+
+video {
+  width: 100%;
+  height: auto;
+}
+</style>
